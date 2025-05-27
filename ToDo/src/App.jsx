da50d5d01a82f35c93f1502518b230df6a8aac13
mejin <wls4482@gmail.com>
2025-05-27 17:34:51 +0900

    chore: 주석 추가

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -5,31 +5,32 @@ import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(() => {
-    const saved = localStorage.getItem("todos");
-    return saved ? JSON.parse(saved) : [];
-  });
+    const saved = localStorage.getItem("todos"); // 저장된 문자열 데이터를 가져온다.
+    return saved ? JSON.parse(saved) : []; // 문자열을 js 객체로 변환한다.
+  }); // 초기 상태 설정: 로컬 스토리지에서 불러오거나 빈 배열
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+  }, [todos]); // 객체를 문자열로 전환하고 로컬스토리지에 문자열을 저장한다.
 
   const addTodo = (text) => {
     const trimmed = text.trim();
     if (!trimmed) return;
-    const newTodo = { id: Date.now(), text: trimmed, completed: false };
-    setTodos([newTodo, ...todos]);
+    const newTodo = { id: Date.now(), text: trimmed, completed: false }; // 시간으로 id생성
+    setTodos([newTodo, ...todos]); // 앞에 새항목 추가
   };
 
   const toggleTodo = (id) => {
     setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      todos.map(
+        (todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo // 완료 상태 토글
       )
     );
   };
 
   const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id)); // 아이디가 일치하지 않는 항목만 남긴다.
   };
 
   return (
--- ToDo/src/components/TodoItem.jsx
+++ ToDo/src/components/TodoItem.jsx
@@ -6,12 +6,13 @@ import {
 } from "../styles/TodoItemStyles.jsx";
 
 function TodoItem({ todo, onToggle, onDelete }) {
+  // 구조 분해 할당
   return (
     <TodoItemStyles>
       <TodoText completed={todo.completed}>{todo.text}</TodoText>
       <div>
         <TodoButton onClick={onToggle}>완료</TodoButton>
-        <TodoButton onClick={onDelete}>삭제</TodoButton>{" "}
+        <TodoButton onClick={onDelete}>삭제</TodoButton> // 트리거
       </div>
     </TodoItemStyles>
   );
