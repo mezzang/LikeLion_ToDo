bf01348242634267d1a0744aaaeae405d6de9ec7
mejin <wls4482@gmail.com>
2025-05-27 17:08:01 +0900

    console.log 제거

--- ToDo/src/components/TodoInput.jsx
+++ ToDo/src/components/TodoInput.jsx
@@ -4,20 +4,21 @@ import { StyledButton } from "../styles/InputButtonStyles.jsx";
 
 function TodoInput({ onAdd }) {
   const [input, setInput] = useState("");
-  const inputRef = useRef(null);
+  const inputRef = useRef(null); //ref객체 생성, DOM요소나 변경 가능한 값 저장
 
   const handleAdd = function () {
-    console.log("handleAdd this:", this);
-    const trimmed = input.trim();
+    const trimmed = input.trim(); // 입력값의 앞뒤 공백 제거
     if (trimmed) {
-      onAdd(trimmed);
-      setInput("");
-      inputRef.current.focus();
+      // 공백이 아닌 경우 onAdd 호출
+      onAdd(trimmed); //할 일 추가
+      setInput(""); // 입력값 초기화
+      inputRef.current.focus(); // 현재 연결된 DOM요소에 접근
     }
   };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
+      // 엔터키를 눌렀을 때
       handleAdd();
     }
   };
@@ -25,10 +26,10 @@ function TodoInput({ onAdd }) {
   return (
     <InputWrapper>
       <StyledTodoInput
-        placeholder="할 일을 입력하세요"
-        value={input}
+        placeholder="할 일을 입력하세요" //html 기본 속성
+        value={input} //상태가 요소의 값 직접 제어
         onChange={(e) => setInput(e.target.value)}
-        onKeyDown={handleKeyDown}
+        onKeyDown={handleKeyDown} //키보드 눌렀을 때 발생
         ref={inputRef}
       />
       {/* <StyledButton onClick={handleAdd}>add</StyledButton> */}
